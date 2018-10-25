5078b197e246702615277dcdb98d3f3de21962fc
vasa-develop <vasa.develop@gmail.com>
2018-10-26 02:28:19 +0530

    fix in https mail server

--- api/controllers/shareController.js
+++ api/controllers/shareController.js
@@ -6,6 +6,8 @@ exports.sendEmail = function(req, res, next) {
 	var subject = req.body.subject;
 	var to = req.body.to;
 
-	cmd.run('echo '+body+' | mail -s '+subject+' '+to+' ');
+	cmd.run('echo \"'+body+'\" | mail -s \"'+subject+'\" '+to+' ');
+
+	res.json({"result": "mail sent successfully"});
 }
 
