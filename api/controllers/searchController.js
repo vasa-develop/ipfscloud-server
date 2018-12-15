e5df2853dbc108b35888ff1635d88cfc2cb717a0
vasa-develop <vasa.develop@gmail.com>
2018-12-15 17:56:36 +0530

    added page paramter

--- api/controllers/searchController.js
+++ api/controllers/searchController.js
@@ -1,9 +1,17 @@
 const fetch = require('node-fetch');
 
 exports.search = function(req, res, next) {
-	fetch('https://api.ipfs-search.com/v1/search?q='+req.query.q)
-    .then(res => res.json())
-    .then(json => res.json(json));
+    if(req.query.page){
+        fetch('https://api.ipfs-search.com/v1/search?q='+req.query.q+'&page='+req.query.page)
+        .then(res => res.json())
+        .then(json => res.json(json));
+    }
+    else{
+        fetch('https://api.ipfs-search.com/v1/search?q='+req.query.q)
+        .then(res => res.json())
+        .then(json => res.json(json));
+    }
+	
 }
 
 
